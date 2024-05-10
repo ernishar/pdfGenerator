3c76221305c89cb7acf85b6b5620ca12505857a3
ernishar <mralamnishar@gmail.com>
2024-05-10 11:09:13 +0530

    initial commit

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config()
 const app = express();
+const pdfRoutes = require('./src/routes/pdfRoutes');
+
+
 const port = 8000;
 
 app.use(express.json());
 
-// Import routes
-const pdfRoutes = require('./src/routes/pdfRoutes');
+
+
 
 // Use routes
 app.use('/api', pdfRoutes);
 
 // app.use(express.urlencoded())
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
